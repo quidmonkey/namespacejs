022ff9669fca973a4a1f7af190988e4c504a875d
Abraham Walters <abraham_walters@epam.com>
2013-11-08 14:34:48 -0500

    Added dependency injection system.

--- namespace.js
+++ namespace.js
@@ -1,8 +1,11 @@
 module = function (namespace, module) {
-    var global = global || window,
+    var args = [],
+        global = global || window,
         key,
         leaf = global,
         name,
+        params,
+        required,
         root,
         tree;
 
@@ -22,12 +25,72 @@ module = function (namespace, module) {
     }
 
     // empty object?
-    for (key in leaf) {
-        if (leaf.hasOwnProperty(key)) {
-            console.log('~~~~ namespacejs: Ruh roh. Potential namespace collision on \'' + namespace +'\'');
-            break;
+    // for (key in leaf) {
+    //     if (leaf.hasOwnProperty(key)) {
+    //         console.log('~~~~ namespacejs: Ruh roh. Potential namespace collision on \'' + namespace +'\'');
+    //         break;
+    //     }
+    // }
+
+    if (module.dependencies[leaf]) {
+        throw '~~~~ namespacejs: Namespace collision on \'' + name +'\' for namespace \'' + namespace + '\'';
+    }
+
+    module.dependencies[leaf] = namespace;
+    
+    params = getParams(module);
+
+    while(params.length) {
+        required = getModule(params.shift());
+        if (required === null) {
+            return hasDependencies(namespace, module);
+        }
+        args.push(required);
+    }
+
+    // module.call(leaf, global, root);
+    module.apply(leaf, args);
+};
+
+checkCache = function () {
+    var cache = module.cache,
+        toLoad;
+
+    module.cache = [];
+
+    while (cache.length) {
+        toLoad = cache.shift();
+        module(toLoad.namespace, toLoad.module);
+    }
+};
+
+getModule = function (leaf) {
+    return module.dependencies[leaf];
+};
+
+getParams = function (module) {
+    var i,
+        params = /\(([^)]+)/.exec(module);
+
+    if (!params[1]) { return []; }
+
+    params = params[1].split(/\s*,\s*/);
+    
+    for (i = params.length; i--;) {
+        if (!/\$/.test(params[i])) {
+            params.splice(i, 1);
         }
     }
 
-    module.call(leaf, global, root);
+    return params;
 };
+
+hasDependencies = function (namespace, module) {
+    module.cache.push({
+        namespace: namespace,
+        module: module
+    });
+};
+
+module.cache = [];
+module.dependencies = {};
